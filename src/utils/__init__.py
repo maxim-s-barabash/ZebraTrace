700839ea1b0ee2134ec61415951b30d621bd46e5
Maxim S. Barabash <maxim.s.barabash@gmail.com>
2012-12-26 08:58:57 +0200

    cleanup

--- src/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import random
-
-
-def id_generator(size=8, chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"):
-	return "".join(random.choice(chars) for x in range(size))
-
-if __name__ == "__main__":
-	print id_generator()
