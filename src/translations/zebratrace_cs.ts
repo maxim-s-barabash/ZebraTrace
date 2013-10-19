651198783c7d9068db4530eabe3220d7c5bdc55f
Maxim <maxim.s.barabash@gmail.com>
2013-10-20 01:18:00 +0300

    Czech translation. Pavel Fric.

---
+++ src/translations/zebratrace_cs.ts
@@ -0,0 +1,462 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="image.py" line="46"/>
+        <source>Desaturate the Image. Press ESC to Cancel.</source>
+        <translation>Odstranit z obrázku sytost. Stiskněte Esc pro zrušení.</translation>
+    </message>
+</context>
+<context>
+    <name>Info</name>
+    <message>
+        <location filename="app.py" line="61"/>
+        <source>Time trace: %5.3f seconds.
+</source>
+        <translation>Časová stopa: %5.3f sekund.
+</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="62"/>
+        <source>Graphic Objects
+  Number of objects: %i
+  Number of nodes: %i
+</source>
+        <translation>Obrazové předměty
+  Počet předmětů: %i
+  Počet uzlů:%i
+</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui_mainwindow.py" line="530"/>
+        <source>Preview:</source>
+        <translation>Náhled:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="532"/>
+        <source>Wireframe Overlay</source>
+        <translation>Překrytí rastrovým obrazem</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="533"/>
+        <source>Transparency:</source>
+        <translation>Průhlednost:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="535"/>
+        <source>Resolution:</source>
+        <translation>Rozlišení:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="556"/>
+        <source>Node Reduction:</source>
+        <translation>Omezení uzlů:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="578"/>
+        <source>Info</source>
+        <translation>Informace</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="583"/>
+        <source>Trace</source>
+        <translation>Vektorizovat</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="593"/>
+        <source>Quit</source>
+        <translation>Ukončit</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="588"/>
+        <source>&amp;File</source>
+        <translation>&amp;Soubor</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="589"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Nápověda</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="590"/>
+        <source>&amp;View</source>
+        <translation>&amp;Pohled</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="591"/>
+        <source>Open Bitmap...</source>
+        <translation>Otevřít obraz...</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="595"/>
+        <source>About...</source>
+        <translation>O programu...</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="596"/>
+        <source>About Qt</source>
+        <translation>O Qt</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="597"/>
+        <source>&amp;Background</source>
+        <translation>&amp;Pozadí</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="598"/>
+        <source>Border &amp;Frame</source>
+        <translation>&amp;Ohraničení</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="599"/>
+        <source>Load Preset...</source>
+        <translation>Nahrát přednastavení...</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="600"/>
+        <source>Save Preset...</source>
+        <translation>Uložit přednastavení...</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="601"/>
+        <source>Trace Image</source>
+        <translation>Vektorizovat obrázek</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="154"/>
+        <source>Open Bitmap File</source>
+        <translation>Otevřít soubor s obrazem</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="210"/>
+        <source>Load Preset File</source>
+        <translation>Nahrát soubor s přednastavením</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="227"/>
+        <source>Preset files (*.preset)</source>
+        <translation>Soubory s přednastavením (*.preset)</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="227"/>
+        <source>Save Preset File</source>
+        <translation>Uložit soubor s přednastavením</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="340"/>
+        <source>About</source>
+        <translation>O programu</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="529"/>
+        <source>ZebraTRACE</source>
+        <translation>ZebraTRACE</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="569"/>
+        <source>Function &lt;b&gt;X(a)&lt;/b&gt;:</source>
+        <translation>Funkce &lt;b&gt;X(a)&lt;/b&gt;:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="571"/>
+        <source>Function &lt;b&gt;Y(a)&lt;/b&gt;:</source>
+        <translation>Funkce &lt;b&gt;Y(a)&lt;/b&gt;:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="586"/>
+        <source>Save</source>
+        <translation>Uložit</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="592"/>
+        <source>Save &amp;As...</source>
+        <translation>Uložit &amp;jako...</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="587"/>
+        <source>Help</source>
+        <translation>Nápověda</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="577"/>
+        <source>Preferences</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="534"/>
+        <source>Transparent bitmap layer</source>
+        <translation>Průhlednost vrstvy rastrového obrazu</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="544"/>
+        <source>Number of Curves (&lt;b&gt;n&lt;/b&gt;):</source>
+        <translation>Počet křivek (&lt;b&gt;n&lt;/b&gt;):</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="557"/>
+        <source>Node reduction tolerance</source>
+        <translation>Dovolená odchylka od omezení uzlů</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="582"/>
+        <source>Trace the raster content</source>
+        <translation>Vektorizovat rastrový obraz</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="585"/>
+        <source>Save trace result to a file</source>
+        <translation>Uložit výsledek vektorizace do souboru</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="192"/>
+        <source>Save File</source>
+        <translation>Uložit soubor</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="531"/>
+        <source>Tracing Result</source>
+        <translation>Výsledek vektorizace</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="154"/>
+        <source>Bitmap files (*.jpg *.jpeg *.png *.gif *.bmp *.tiff *.tga);;All files (*.*)</source>
+        <translation>Obrazové soubory (*.jpg *.jpeg *.png *.gif *.bmp *.tiff *.tga);;Všechny soubory (*.*)</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="167"/>
+        <source>Open file</source>
+        <translation>Otevřít soubor</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="167"/>
+        <source>This file is corrupt or not supported?</source>
+        <translation>Tento soubor je poškezen nebo není podporován?</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="415"/>
+        <source>Error in function</source>
+        <translation>Chyba ve funkci</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="543"/>
+        <source> Curves</source>
+        <translation>Křivky</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="558"/>
+        <source>Advanced</source>
+        <translation>Pokročilé</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="559"/>
+        <source>Functions</source>
+        <translation>Funkce</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="572"/>
+        <source>Trace result details</source>
+        <translation>Podrobnosti výsledku vektorizace</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="566"/>
+        <source>Coordinates:</source>
+        <translation>Souřadnice:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="567"/>
+        <source>Cartesian</source>
+        <translation>Kartézské</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="568"/>
+        <source>Polar</source>
+        <translation>Polární</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="573"/>
+        <source>Paths:</source>
+        <translation>Cesty:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="575"/>
+        <source>Nodes:</source>
+        <translation>Uzly:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="546"/>
+        <source>Minimum Width:</source>
+        <translation>Nejmenší šířka:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="548"/>
+        <source>Maximum Width:</source>
+        <translation>Největší šířka:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="551"/>
+        <source>Center Line</source>
+        <translation>Střední čára</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="552"/>
+        <source>Left End</source>
+        <translation>Ploška zleva</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="553"/>
+        <source>Right End</source>
+        <translation>Ploška zprava</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="554"/>
+        <source>Left Line</source>
+        <translation>Čára zleva</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="555"/>
+        <source>Right Line</source>
+        <translation>Čára zprava</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="550"/>
+        <source>Writing:</source>
+        <translation>Podoba:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="560"/>
+        <source>Quality:</source>
+        <translation>Kvalita:</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="192"/>
+        <source>SVG files (*.svg);;NGC g-code files (*.ngc)</source>
+        <translation>Soubory SVG (*.svg);;Soubory NGC g-code (*.ngc)</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="537"/>
+        <source>Units:</source>
+        <translation>Jednotky:</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="539"/>
+        <source>px</source>
+        <translation>px</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="540"/>
+        <source>mm</source>
+        <translation>mm</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="541"/>
+        <source>cm</source>
+        <translation>cm</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="542"/>
+        <source>in</source>
+        <translation>palce</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="545"/>
+        <source>&lt;b&gt;i&lt;/b&gt; = 1... &lt;b&gt;n&lt;/b&gt;</source>
+        <translation>&lt;b&gt;i&lt;/b&gt; = 1... &lt;b&gt;n&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="549"/>
+        <source>In the current unit</source>
+        <translation>V nynější jednotce</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="561"/>
+        <source>Percentage</source>
+        <translation>Procento</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="562"/>
+        <source>Min (&lt;b&gt;a&lt;/b&gt;) value:</source>
+        <translation>Nejmenší hodnota (&lt;b&gt;a&lt;/b&gt;):</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="563"/>
+        <source>Minimum value for (a) variable</source>
+        <translation>Nejmenší hodnota pro proměnnou (a)</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="564"/>
+        <source>Max (&lt;b&gt;a&lt;/b&gt;) value:</source>
+        <translation>Největší hodnota (&lt;b&gt;a&lt;/b&gt;):</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="565"/>
+        <source>Maximum value for (a) variable</source>
+        <translation>Největší hodnota pro proměnnou (a)</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="570"/>
+        <source>Use (a),( i) and (n) as parameters</source>
+        <translation>Použít (a),( i) a (n) jako parametry</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="576"/>
+        <source>0</source>
+        <translation>0</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="579"/>
+        <source>%p%</source>
+        <translation>%p%</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="584"/>
+        <source>Return</source>
+        <translation>Enter</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="594"/>
+        <source>Ctrl+X</source>
+        <translation>Ctrl+X</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="538"/>
+        <source>Drawing Units</source>
+        <translation>jednotky kreslení</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="536"/>
+        <source>Dots per inch in Image</source>
+        <translation>Teček na palec v obrázku</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="580"/>
+        <source>(0.000;0.000)</source>
+        <translation>(0.000;0.000)</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="460"/>
+        <source>Simplify Points</source>
+        <translation>Zjednodušit body</translation>
+    </message>
+    <message>
+        <location filename="app.py" line="336"/>
+        <source>&lt;center&gt;&lt;b&gt;%s&lt;/b&gt; version %s. &lt;br&gt;&lt;br&gt;
+See &lt;a href=&quot;http://maxim-s-barabash.github.io/ZebraTrace/&quot;&gt;ZebraTrace&lt;/a&gt;
+for more information.&lt;br&gt;&lt;br&gt;
+Copyright (C) 2012-2013&lt;/center&gt;</source>
+        <translation>&lt;center&gt;&lt;b&gt;%s&lt;/b&gt; verze %s. &lt;br&gt;&lt;br&gt;
+Podívejte se na &lt;a href=&quot;http://maxim-s-barabash.github.io/ZebraTrace/&quot;&gt;ZebraTrace&lt;/a&gt;,
+kde najdete další informace.&lt;br&gt;&lt;br&gt;
+Autorské právo (C) 2012-2013&lt;/center&gt;</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="581"/>
+        <source>Message</source>
+        <translation>Zpráva</translation>
+    </message>
+</context>
+</TS>
